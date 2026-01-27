bb7378ebb7190a1c7c2af2adcd815ddb28460d58
giruml34 <girumw@uw.edu>
2026-01-27 13:51:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+map.on('load', () => {
+    map.addSource('covid-rates', {
+        type: 'geojson',
+        data: 'assets/us-covid-2020-rates.geojson'
+    });
+
+    map.addLayer({
+        id: 'covid-rates-layer',
+        type: 'fill',
+        source: 'covid-rates'
+    });
+});
 
