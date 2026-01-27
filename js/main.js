a6c9ebe33795ad72750df2f968263b21a9c1186f
giruml34 <girumw@uw.edu>
2026-01-27 14:35:03 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lydW13IiwiYSI6ImNtaGNsMnczejI4a2cybXB1b3h6dHBuaHkifQ.fO5Cyk2RL57zq0RKG8BDkg';
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-98, 38],
-    zoom: 3,
-    projection: 'albers'
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [-98, 38],
+  zoom: 3,
+  projection: 'albers'
 });
 
 map.on('load', () => {
-    map.addSource('covid-rates', {
-        type: 'geojson',
-        data: 'assets/us-covid-2020-rates.geojson'
-    });
 
-    map.addLayer({
-        id: 'covid-rates-layer',
-        type: 'fill',
-        source: 'covid-rates',
-        paint: {
-            'fill-color': [
-                'interpolate',
-                ['linear'],
-                ['get', 'rates'],
-                0, '#f7fbff',
-                10, '#c6dbef',
-                25, '#6baed6',
-                50, '#2171b5',
-                100, '#08306b'
-            ],
-            'fill-opacity': 0.8,
-            'fill-outline-color': '#ffffff'
-        }
-    });
+  // ===== COVID RATES (map1.html) =====
+  map.addSource('covid-rates', {
+    type: 'geojson',
+    data: 'assets/us-covid-2020-rates.geojson'
+  });
+
+  map.addLayer({
+    id: 'covid-rates-layer',
+    type: 'fill',
+    source: 'covid-rates',
+    paint: {
+      'fill-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'rate'],
+        0, '#f7fbff',
+        5, '#c6dbef',
+        10, '#6baed6',
+        20, '#2171b5',
+        40, '#08306b'
+      ],
+      'fill-opacity': 0.8
+    }
+  });
+
+  map.on('click', 'covid-rates-layer', (e) => {
+    new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML(
+        `<strong>County:</strong> ${e.features[0].properties.county}<br>
+         <strong>Case Rate:</strong> ${e.features[0].properties.rate}`
+      )
+      .addTo(map);
+  });
+
 });
