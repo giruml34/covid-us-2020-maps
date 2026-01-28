6bb0348b11e8681a0a3d51b413deeb4220b984dd
giruml34 <girumw@uw.edu>
2026-01-27 17:41:03 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,85 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lydW13IiwiYSI6ImNtaGNsMnczejI4a2cybXB1b3h6dHBuaHkifQ.fO5Cyk2RL57zq0RKG8BDkg';
 
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-98, 38],
-  zoom: 3,
-  projection: 'albers'
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-98, 38],
+    zoom: 3
 });
 
 map.on('load', () => {
 
-  // ===== COVID RATES (map1.html) =====
-  map.addSource('covid-rates', {
-    type: 'geojson',
-    data: 'assets/us-covid-2020-rates.geojson'
-  });
+    /* ---------- CHOROPLETH (RATES) ---------- */
+    map.addSource('rates', {
+        type: 'geojson',
+        data: 'assets/us-covid-2020-rates.geojson'
+    });
 
-  map.addLayer({
-    id: 'covid-rates-layer',
-    type: 'fill',
-    source: 'covid-rates',
-    paint: {
-      'fill-color': [
-        'interpolate',
-        ['linear'],
-        ['get', 'rate'],
-        0, '#f7fbff',
-        5, '#c6dbef',
-        10, '#6baed6',
-        20, '#2171b5',
-        40, '#08306b'
-      ],
-      'fill-opacity': 0.8
-    }
-  });
+    map.addLayer({
+        id: 'rates-layer',
+        type: 'fill',
+        source: 'rates',
+        paint: {
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'rates'],
+                0, '#edf8fb',
+                10, '#b3cde3',
+                20, '#8c96c6',
+                30, '#8856a7',
+                40, '#810f7c'
+            ],
+            'fill-opacity': 0.8
+        }
+    });
 
-  map.on('click', 'covid-rates-layer', (e) => {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML(
-        `<strong>County:</strong> ${e.features[0].properties.county}<br>
-         <strong>Case Rate:</strong> ${e.features[0].properties.rate}`
-      )
-      .addTo(map);
-  });
+    /* ---------- PROPORTIONAL SYMBOLS (CASES) ---------- */
+    map.addSource('cases', {
+        type: 'geojson',
+        data: 'assets/us-covid-2020-counts.geojson'
+    });
 
+    map.addLayer({
+        id: 'cases-layer',
+        type: 'circle',
+        source: 'cases',
+        paint: {
+            'circle-radius': [
+                'interpolate',
+                ['linear'],
+                ['get', 'cases'],
+                1000, 3,
+                10000, 8,
+                50000, 16,
+                200000, 30
+            ],
+            'circle-color': '#41b6c4',
+            'circle-opacity': 0.6,
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1
+        }
+    });
+
+    /* ---------- POPUP ---------- */
+    map.on('click', 'cases-layer', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(
+                `<strong>County:</strong> ${e.features[0].properties.county}<br>
+                 <strong>Cases:</strong> ${e.features[0].properties.cases}`
+            )
+            .addTo(map);
+    });
+
+    /* ---------- LEGEND ---------- */
+    const legend = document.getElementById('legend');
+    legend.innerHTML = `
+        <strong>Legend</strong><br>
+        <div class="break"><span class="dot" style="width:10px;height:10px;background:#edf8fb"></span>Low</div>
+        <div class="break"><span class="dot" style="width:14px;height:14px;background:#8c96c6"></span>Medium</div>
+        <div class="break"><span class="dot" style="width:18px;height:18px;background:#810f7c"></span>High</div>
+        <hr>
+        <small>Source: NY Times & ACS</small>
+    `;
 });
